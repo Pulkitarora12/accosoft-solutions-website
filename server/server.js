2b9e40b1a162ea7c0120fd1e75959dc87ec6c8bc
Ankur Gagneja <ankurgagneja21@gmail.com>
2026-08-08 16:08:23 +0530

    trust proxy updation

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ import { initDb, openDb } from './database.js';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 const __filename = fileURLToPath(import.meta.url);
